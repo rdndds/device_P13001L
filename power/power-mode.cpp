4f682485f8cf65b8627f42ced8063c302d3cc296
rdndds <ardiandideyashidiq@gmail.com>
2025-12-10 11:29:54 +0700

    disable dt2w

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -19,25 +19,12 @@ namespace mediatek {
 using ::aidl::android::hardware::power::Mode;
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
-    switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE:
-            *_aidl_return = true;
-            return true;
-        default:
-            return false;
-    }
+    *_aidl_return = false;
+    return false;
 }
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
-    switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE: {
-            ::android::base::WriteStringToFile(enabled ? "1" : "0", "/proc/android_touch/GESTURE", true);
-            ::android::base::WriteStringToFile(enabled ? "1" : "0", "/proc/android_touch/SMWP", true);
-            return true;
-        }
-        default:
-            return false;
-    }
+    return false;
 }
 
 }  // namespace mediatek
